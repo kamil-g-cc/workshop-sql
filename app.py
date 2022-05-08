6016bb2fc3015cf430705890fd84825583057e1e
Kamil Grondys <kamil.grondys@codecool.com>
2022-05-08 13:42:39 +0200

    serach results

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import psycopg2
 app = Flask(__name__)
 
@@ -15,6 +15,28 @@ def hello_world():  # put application's code here
             <input type="submit" value="Szukaj">
         </form>
     """
+
+@app.route('/search_results')
+def search():
+    out = ""
+    search_phrase = request.args.get("query")
+    connection = psycopg2.connect("dbname=nw_traders user=kamil password=test123")
+    cursor = connection.cursor()
+    sql_query = "SELECT product_name FROM products WHERE product_name LIKE '"+search_phrase+"%'"
+    print(sql_query)
+    cursor.execute(sql_query)
+    data = cursor.fetchall()
+
+    for row in data:
+        out+="<tr><td>" + row[0] + "</td></tr>"
+
+    return """<h1>Witaj</h1>
+            <h2>Wyniki wyszukiwania</h2>
+            <table>
+                <tr><th>Nazwa produktu</th></tr>
+                """+ out +"""
+            </table>
+        """
 
 
 if __name__ == '__main__':
